c283ab61c1738a2fbca5ca9010e8346aeac67ac1
moh112004 <almstym870@gmail.com>
2024-06-21 18:46:25 +0300

    my site

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,3 @@
-import me from "../me.jpg";
 import "../Project.css"
 let AStyle = {
 textDecoration: "none",
@@ -6,17 +5,16 @@ color: "#eee",
 borderBottom: "1px #eee solid",
 fontWeight: "500"
 };
-export default function Project() {
+export default function Project({image, lable, content, link}) {
   return (
     <>
       <div className="project">
-        <img src={me} alt="" />
-        <h3 style={{margin: "0", fontSize: "20px"}}>Lable</h3>
+        <img src={image} alt="" />
+        <h3 style={{margin: "0", fontSize: "20px"}}>{lable}</h3>
         <p>
-          lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-          lorem lorem lorem lorem lorem
+          {content}
         </p>
-        <span>Project's link : </span><a href="https://google.com" style={AStyle}>google.com</a>
+        <span>Project's link : </span><a href={"https://"+link} style={AStyle}>{link}</a>
       </div>
     </>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,33 +1,53 @@
 import { useState } from "react";
-import "../Projects.css"
+import "../Projects.css";
 import Project from "./Project";
-
+import leon from "../leon.jpg";
+import rajil from "../rajil.jpg";
+import rajilCaptain from "../rajil-captain.jpg";
+import rajilClient from "../rajil-client.jpg";
+import moda from "../moda.jpg";
 export default function Projects() {
   let [more, setMore] = useState(0);
   let [buttonTitle, setButtonTitle] = useState("Show more ...");
   return (
     <>
       <div className="projectsRow">
-        <Project />
-        <Project />
-        <Project />
-        <Project />
+        <Project
+          image={moda}
+          lable="Moda steel factory"
+          content="An introductory website for Moda Steel Factory."
+          link="modasteelfactory.com"
+        />
+        <Project
+          image={rajilClient}
+          lable="Rajil client form"
+          content="A page that is a form that is part of an order delivery system that the customer fills out when he orders."
+          link="rajil-client-form.onrender.com"
+        />
+        <Project
+          image={rajilCaptain}
+          lable="Rajil captain form"
+          content="A page that is a form that is part of an order delivery system that the captain fills out when he delivers the order."
+          link="rajil-captain-form.onrender.com"
+        />
+        <Project
+          image={rajil}
+          lable="Rajil soon page"
+          content="'soon' page for Rajil Delivery Company's website."
+          link="rajil.onrender.com"
+        />
       </div>
       <div
         className="more"
         style={more === 0 ? { display: "none" } : { display: "block" }}
       >
         <div className="projectsRow">
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-        </div>
-        <div className="projectsRow">
-          <Project />
-          <Project />
-          <Project />
-          <Project />
+          <Project
+            image={leon}
+            lable="Leon"
+            content="Blank single page application template for reuse."
+            link="leon-n8kz.onrender.com"
+          />
         </div>
       </div>
       <span onClick={MoreClicked} className="moreButton">
