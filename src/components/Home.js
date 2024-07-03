8e9496d27b004b867637826c48aa9a4ae341fdae
moh112004 <almstym870@gmail.com>
2024-07-03 09:18:49 +0300

    df

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,55 @@
 // Home.js
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 import AppBar from "./AppBar";
-import Landing from "./Landing";
+// import Landing from "./Landing";
 import Lable from "./Lable";
 import About from "./About";
 // import Articles from "./Articles";
 import Projects from "./Projects";
-import Services from "./Services"
+import Services from "./Services";
 import Contact from "./Contact";
-import WhatsAppIcon from "./WhatsAppIcon";
+// import WhatsAppIcon from "./WhatsAppIcon";
 import Footer from "./Footer";
-import "../Home.css"
+import "../Home.css";
+const Landing = React.lazy(() => import('./Landing'));
+const WhatsAppIcon = React.lazy(() => import('./WhatsAppIcon'));
+
 function Home() {
   return (
-        <Suspense fallback={<div>Loading...</div>}>
-    <div>
-      <AppBar />
-      <div className="home">
-
-        <Landing />
-        <span id="about">
-          <Lable>About</Lable>
-        </span>
-        <About />
-        <span id="portfolio">
-          <Lable>Portfolio</Lable>
-        </span>
-        <Projects />
-        {/* <span id="blog">
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <AppBar />
+        <div className="home">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Landing />
+          </Suspense>
+          <span id="about">
+            <Lable>About</Lable>
+          </span>
+          <About />
+          <span id="portfolio">
+            <Lable>Portfolio</Lable>
+          </span>
+          <Projects />
+          {/* <span id="blog">
           <Lable>Blog</Lable>
         </span> 
-        <Articles />*/}  
-        <span id="services">
-          <Lable>Services</Lable>
-        </span>
-        <Services />
-        <span id="contact">
-          <Lable>Contact</Lable>
-        </span>
-        <Contact />
+        <Articles />*/}
+          <span id="services">
+            <Lable>Services</Lable>
+          </span>
+          <Services />
+          <span id="contact">
+            <Lable>Contact</Lable>
+          </span>
+          <Contact />
+        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <WhatsAppIcon />
+        </Suspense>
+        <Footer />
       </div>
-      <WhatsAppIcon/>
-      <Footer/>
-    </div>
-      </Suspense>
+    </Suspense>
   );
 }
 
