997c8a3ea6c6f968e600c91a20163c06633dca53
moh112004 <almstym870@gmail.com>
2024-07-19 06:26:58 +0300

    push

--- src/components/About.js
+++ src/components/About.js
@@ -4,25 +4,25 @@ import "../About.css";
 import MarkdownRenderer from "../MarkdownRenderer";
 
 const markdownContent = `
-# Mohammed Elmostafa  
-**Front-End Developer**
+# Mohammed Elmostafa
+**Frontend Developer**
 
 ## Summary
-Hello, I'm Mohammed Elmostafa, a Front-End Developer specializing in performance optimization, user experience, and interactive design. Despite my short experience of less than a year, I have successfully completed several freelance projects and held leadership roles.
+Hello, I'm Mohammed Elmostafa, a frontend developer specializing in performance optimization, user experience, and interactive design. Although my professional experience is less than a year, I have successfully completed many projects.
 
 ## Education
-**Self-Learning**  
-Continuously expanding my knowledge through self-learning and staying updated with the latest industry trends.
+### Self-Learning
+I continuously expand my knowledge through self-learning and staying updated with the latest industry trends.
 
 ## Professional Experience
-**CEO and Co-Founder, Eltech**  
-Leading a tech startup, overseeing technical and strategic initiatives.
+### CEO and Co-Founder, ELTECH Company
+I lead the tech company, overseeing strategic and technical initiatives.
 
-**Freelance Front-End Developer**  
-Developed several successful projects for clients, demonstrating expertise in creating performant and user-friendly web applications.
+### Freelance Frontend Developer
+I worked on developing a number of successful projects that were approved for clients.
 
-**Technical Sponsor**  
-Provided technical sponsorship for several events, including:
+### Technical Sponsor
+I have provided technical sponsorship for several events, including:
 - Takaful Forum
 - VMC5 Program
 - YDC23
@@ -30,43 +30,43 @@ Provided technical sponsorship for several events, including:
 
 ## Skills
 - **Core Skills:** HTML, CSS, JavaScript, Bootstrap, React.js
-- **Additional Skills:** Performance optimization, User Experience (UX), Interactive design, Research and analysis, Problem-solving, Individual and team work, Flexibility and fast learning
+- **Additional Skills:** Performance optimization, User Experience (UX), Interactive design, Research and analysis, Problem-solving, Individual and team work, Flexibility and quick learning
 - **Tools:** Git, GitHub, Command Line (CMD)
 
-## Competitions
-**IEEE Xtreme Competition**  
-Participated in IEEE Xtreme 16 and 17, showcasing problem-solving skills and coding proficiency in a competitive environment.
+## Activities
+### IEEExtreme Competition
+I participated in the 16th and 17th editions of the IEEExtreme competition.
 
 ## Interests
-I am passionate about improving website performance and user experience. I enjoy designing interactive and appealing interfaces and constantly strive to enhance my skills and stay updated with the latest trends in web development.
+I am passionate about improving website performance and user experience. I enjoy designing interactive and attractive interfaces and constantly work on enhancing my skills and staying informed about the latest web development trends.
 
 ## Projects
-I have some previous work you can see in the portfolio section.
+You can check out some of my previous work in the [projects](#portfolio) section.
 
 ## Contact
-If you would like to get in touch to discuss a project or a job opportunity, you can reach me through the [Contact](#contact) section. I am always happy to receive inquiries and collaborate on new projects.
+If you would like to discuss a project or job opportunity, you can reach me through the [contact](#contact) section. I am always open to inquiries and collaboration on new projects.
 `;
 const markdownContentAr = `
 # محمد المصطفى
 **مطور واجهات أمامية**
 
-## ملخص
-مرحباً، أنا محمد المصطفى، مطور واجهات أمامية متخصص في تحسين الأداء وتجربة المستخدم والتصميم التفاعلي. رغم أن تجربتي القصيرة أقل من سنة، فقد أتممت بنجاح العديد من المشاريع الحرة وتوليت أدواراً قيادية.
+## نبذة
+مرحباً، أنا محمد المصطفى، مطور واجهات أمامية متخصص في تحسين الأداء وتجربة المستخدم والتصميم التفاعلي. رغم أن تجربتي المهنية لا تتجاوز السنة، فقد أنجزت العديد من المشاريع بنجاح.
 
 ## التعليم
-**التعلم الذاتي**  
-أعمل على توسيع معرفتي باستمرار من خلال التعلم الذاتي والبقاء محدثاً بآخر اتجاهات الصناعة.
+### التعلم الذاتي
+أحرص على توسيع معرفتي باستمرار من خلال التعلم الذاتي ومتابعة آخر مستجدات الصناعة.
 
 ## الخبرة المهنية
-**المدير التنفيذي والشريك المؤسس، شركة إلتك**  
-قيادة شركة تقنية، والإشراف على المبادرات التقنية والاستراتيجية.
+### المدير التنفيذي والشريك المؤسس، شركة إلتك
+أشرف على قيادة الشركة التقنية وتوجيه المبادرات الاستراتيجية والتقنية.
 
-**مطور واجهات أمامية حر**  
-طورت عدة مشاريع ناجحة للعملاء، مما يدل على خبرتي في إنشاء تطبيقات ويب ذات أداء عالي وتجربة مستخدم ممتازة.
+### مطور واجهات أمامية فريلانسر
+عملت على تطوير عدد من المشاريع الناجحة و التي تم إعتمادها للعملاء.
 
-**الراعي التقني**  
-قدمت رعاية تقنية للعديد من الفعاليات، بما في ذلك:
-- منتدى التكافل
+### الراعي التقني
+قدمت الدعم التقني للعديد من الفعاليات، بما في ذلك:
+- ملتقى تكافل
 - برنامج VMC5
 - YDC23
 - TEDxMST
@@ -76,18 +76,19 @@ const markdownContentAr = `
 - **المهارات الإضافية:** تحسين الأداء، تجربة المستخدم (UX)، التصميم التفاعلي، البحث والتحليل، حل المشكلات، العمل الفردي والجماعي، المرونة والتعلم السريع
 - **الأدوات:** Git، GitHub، سطر الأوامر (CMD)
 
-## المسابقات
-**مسابقة IEEE Xtreme**  
-شاركت في مسابقة IEEE Xtreme 16 و17، مما يظهر مهاراتي في حل المشكلات والبرمجة في بيئة تنافسية.
+## الأنشطة
+### مسابقة IEEExtreme
+شاركت في النسختين 16 و17 من مسابقة IEEExtreme.
 
 ## الاهتمامات
-أنا شغوف بتحسين أداء المواقع وتجربة المستخدم. أستمتع بتصميم واجهات تفاعلية وجذابة وأعمل باستمرار على تحسين مهاراتي والبقاء على اطلاع بآخر اتجاهات تطوير الويب.
+أهتم بتحسين أداء المواقع وتجربة المستخدم، وأستمتع بتصميم واجهات تفاعلية وجذابة. أعمل باستمرار على تطوير مهاراتي ومتابعة أحدث اتجاهات تطوير الويب.
 
 ## المشاريع
-لدي بعض الأعمال السابقة التي يمكنك الاطلاع عليها في قسم المحفظة.
+لدي بعض الأعمال السابقة التي يمكنك الاطلاع عليها في قسم [الأعمال](#portfolio).
 
 ## التواصل
-إذا كنت ترغب في التواصل لمناقشة مشروع أو فرصة عمل، يمكنك الوصول إليّ عبر قسم التواصل. أنا دائماً سعيد بتلقي الاستفسارات والتعاون في مشاريع جديدة.
+إذا كنت ترغب في مناقشة مشروع أو فرصة عمل، يمكنك التواصل معي عبر قسم [التواصل](#contact). أنا دائماً مستعد للاستفسارات والتعاون في مشاريع جديدة.
+
 
 `;
 export default function About() {
